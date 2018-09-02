4672eb128773e04ecab609c2c9a8881183171a10
Alexis Lefebvre <alexislefebvre+github@gmail.com>
2018-09-02 22:20:17 +0200

    Add unit test

--- polls/tests.py
+++ polls/tests.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.utils import timezone
+
 from django.test import TestCase
 
-# Create your tests here.
+from polls.models import Choice, Question
+
+class QuestionTestCase(TestCase):
+    def setUp(self):
+        Question.objects.create(question_text="What's up?", pub_date=timezone.now())
+
+    def test_question_text(self):
+        """Question"""
+        question = Question.objects.get(question_text="What's up?")
+        self.assertEqual(question.question_text, 'What\'s up?')
